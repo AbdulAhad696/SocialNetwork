812082191fedf9706eb1ceebfe4f909aa46cfaee
AbdulAhad696 <76742556+AbdulAhad696@users.noreply.github.com>
2023-01-14 13:06:29 +0500

    Delete backend directory

--- backend/utility/constants.js
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    baseUrl:"http://localhost:8001",
-    baseUrlFronrEnd:"http://localhost:4200"
-  };
-export const pvtkey = "thefourhorsemen";
